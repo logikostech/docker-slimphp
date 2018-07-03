19f8aeb5f5d6940541f251e136aaa47cc51c9799
tempcke <t.bitbucket@logikostech.net>
2018-07-02 22:33:33 -0500

    created a class to implement app, though it adds nothing else yet

---
+++ app/src/Slim/LtApp.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Logikos\Slim;
+
+/**
+ * Class App
+ * @package Logikos\Slim
+ *
+ * The purpose of this class is only to implement the interface so that things can be mocked and tested...
+ */
+class LtApp extends \Slim\App implements App {
+
+}
